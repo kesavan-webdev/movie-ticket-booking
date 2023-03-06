78cc6dff86aec1425f4be24762f21d14226e950e
kesavan <kesavanplk7@gmail.com>
2023-03-06 14:11:53 +0530

    add styles and get elements  in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
+//elements
 
+const movie = document.getElementById("movie-container");
+const seat = document.querySelectorAll(".row .seat:not(.booked)"); //when we use queryselector only it gives 1st element only if we use queryselecterAll it give all elements in node list
+
+console.log(movie);
+console.log(seat);
+
+//global variables
+
+let ticketPrice = 0;
+let ticketCounts = 0;
+let totalFare = 0;
+//events
+
+movie.addEventListener("change", (e) => {
+  console.log(e);
+  ticketPrice = Number(e.target.value);
+  console.log(ticketPrice);
+});
+
+seat.addEventListener("click", (e) => {
+  e.target;
+  console.log(e.target);
+});
