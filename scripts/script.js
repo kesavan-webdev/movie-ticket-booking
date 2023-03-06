c0f8677dd80a02bd95b69f8e8957adcf9f84514f
kesavan <kesavanplk7@gmail.com>
2023-03-06 22:30:48 +0530

    add logic in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,52 @@
 //elements
 
 const movie = document.getElementById("movie-container");
-const seat = document.querySelectorAll(".row .seat:not(.booked)"); //when we use queryselector only it gives 1st element only if we use queryselecterAll it give all elements in node list
+const seats = document.querySelectorAll(".row .seat:not(.booked)");
+const total = document.getElementById("total");
+const counts = document.getElementById("counts"); //when we use queryselector only it gives 1st element only if we use queryselecterAll it give all elements in node list
 
-console.log(movie);
-console.log(seat);
+// console.log(movie);
+// console.log(seats);
+// console.log(total);
+// console.log(counts);
 
 //global variables
 
 let ticketPrice = 0;
 let ticketCounts = 0;
 let totalFare = 0;
+
+//function
+
+function updateCounts() {
+  let selectedSeats = document.querySelectorAll(".row .selected");
+  ticketCounts = selectedSeats.length;
+  // console.log(ticketCounts);
+  totalFare = ticketCounts * ticketPrice;
+  total.innerText = totalFare;
+  counts.innerText = ticketCounts;
+}
+
 //events
 
 movie.addEventListener("change", (e) => {
-  console.log(e);
+  // console.log(e);
   ticketPrice = Number(e.target.value);
-  console.log(ticketPrice);
+  // console.log(ticketPrice);
+  updateCounts();
 });
+for (let i = 0; i < seats.length; i++) {
+  const seatList = seats[i];
 
-seat.addEventListener("click", (e) => {
-  e.target;
-  console.log(e.target);
-});
+  seatList.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target.classList.contains("seat")) {
+      e.target.classList.toggle("selected");
+      // console.log(e.target.classList.contains("selected"));
+      updateCounts();
+      // console.log(e.target.value);
+    } //  else if (e.target.classList.contains("seat selected")) {
+    //   let SeatSelected = e.target.classList.remove("seat");
+    // }
+  });
+}
